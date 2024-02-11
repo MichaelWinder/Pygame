686dd38167551646ed58a60d715e38d887d02979
MichaelWinder <winderm2@middleton.school.nz>
2024-02-12 10:06:23 +1300

    test

--- Snakey_game.py
+++ Snakey_game.py
@@ -1,6 +1,13 @@
 import pygame
+import random
+import time
 pygame.init()
 
+height = random.randrange(200, 1000, 20)
+length = random.randrange(200, 1500, 20)
+screen = pygame.display.set_mode((length, height))
 
-pygame.quit()
-quit()
+time.sleep(99999)
+
+# pygame.quit()
+# quit()
