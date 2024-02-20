7706aab45ebfb4d8d26f22fbedc4bad159f81f83
MichaelWinder <winderm2@middleton.school.nz>
2024-02-21 10:13:12 +1300

    snake_12

--- Snakey_game_12.py
+++ Snakey_game_12.py
@@ -22,18 +22,47 @@ purple = (128,0,128)
 # Fonts for the game
 pause_font_size = int(length/18)
 exit_font_size = int(length/12)
-score_font = pygame.font.Font("CHILLER.TTF", 90)
+score_font = pygame.font.Font("CHILLER.TTF", 50)
 exit_font = pygame.font.Font("CHILLER.TTF", exit_font_size)
 pause_font = pygame.font.Font("impact.ttf", pause_font_size)
 
 # Sets the speed for the snake
 clock = pygame.time.Clock()
 
+# Function to keep track of the highest score - writes value to a file
+def load_high_score():
+    try:
+        hi_score_file = open("HI_score.txt", 'r')
+    except IOError:
+        hi_score_file = open("HI_score.txt", 'w')
+        hi_score_file.write("0")
+    hi_score_file = open("HI_score.txt", 'r')
+    value = hi_score_file.read()
+    hi_score_file.close()
+    return value
+
+# Function to update record of the highest score
+def update_high_score(score, high_score):
+    if int(score) > int(high_score):
+        return score
+    else:
+        return high_score
+
+# Save updated high score if player beats it
+def save_high_score(high_score):
+    high_score_file = open("HI_score.txt", 'w')
+    high_score_file.write(str(high_score))
+    high_score_file.close()
+
 # Display player score throughout the game
-def player_score(score, score_colour):
+def player_score(score, score_colour, hi_score):
     display_score = score_font.render(f"Score: {score}", True, score_colour)
-    screen.blit(display_score, (20, 20)) # Coordinates for top
-    # right
+    screen.blit(display_score, (5, 0)) # Coordinates for top left
+    # High score
+    display_score = score_font.render(f"High Score: {hi_score}", True,
+                                      score_colour)
+    screen.blit(display_score, (5, 40)) # Coordinates for top left lower down
+
 
 # Create snake - replaces the previous snake drawing section in main loop
 def draw_snake(snake_list):
@@ -57,6 +86,7 @@ def snake_direction_check(s_dir, new_dir):
         return new_dir
 
 def game_loop():
+    start_time = time.time() # To record score (time) form start of game
     quit_game = False
     game_over = False
 
@@ -80,9 +110,13 @@ def game_loop():
     food_x = round(random.randrange(20, length - 20) / 20) * 20
     food_y = round(random.randrange(20, height - 20) / 20) * 20
 
+    # Load the high score
+    high_score = load_high_score()
+
     while not quit_game:
         # Give user the option to quit or play again when they die
         while game_over:
+            save_high_score(high_score)
             screen.fill(black)
             message("Press 'Q' to Quit or 'A' to play Again", red,
                     black, exit_font)
@@ -184,15 +218,18 @@ def game_loop():
 
         # Keeping track of the player's score
         score = snake_length - 1
-        player_score(score, purple)
+        player_score(score, purple, high_score)
 
         pygame.display.update()
 
+        # Get high score
+        high_score = update_high_score(score, high_score)
+
         # Link speed of snake to player score in increase difficulty
-        if score > 3:
+        if score > 10:
             speed = score
         else:
-            speed = 3
+            speed = 10
 
         # Collision dectection with food
         if snake_x == food_x and snake_y == food_y:
